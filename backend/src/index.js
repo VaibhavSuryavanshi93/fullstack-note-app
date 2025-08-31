1751d3bc8dd7b600896d3c6055a97cb98cf76cc9
Vaibhav Suryavanshi <128031884+VaibhavSuryavanshi93@users.noreply.github.com>
2025-09-01 03:16:59 +0530

    path error

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,10 +26,11 @@ app.use("/api/notes", noteRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("/*", (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
+
 
 app.listen(PORT, () => {
   console.log(`server is running on http://localhost:${PORT}`);
